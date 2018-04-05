a0e029e5a53290548a1e069b4c03c7bf4c88a7a7
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 20:22:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-//var katzDeli = [];
+var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name) 
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  var name = katzDeli.shift()
+  return name
 }
