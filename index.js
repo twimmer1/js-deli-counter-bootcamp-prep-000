4b2a901f4d385325364452eb003a6d6f4428867a
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 20:05:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   for (var i = 0; i < 7; i++) {
-    if (katzDeliLine === name) {
+    if (katzDeliLine[i] === name) {
     return `Welcome, ${name}. You are number ${i + 1} in line.`
     }
   }
