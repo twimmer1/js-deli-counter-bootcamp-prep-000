b6e1d9224834700de68aae3d34790e577e18e7cd
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 20:33:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeli) {
 }
 
 function currentLine(katzDeliLine) {
-  if(katzDeliLine === katzDeliLine.length) {
+  if(katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
   var result = "The line is currently: "
