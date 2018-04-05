60434f5a6afb97b761f4fc26259d5f17afd7b16d
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 20:17:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name) 
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line."`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
